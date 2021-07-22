cf6d963911e26fbf233ddbd44c03f2c5d9dd9d98
Azhar Smagulova <azharsmagulova@unist.ac.kr>
2021-07-22 17:41:21 +0900

    Helper functions added & Subtraction started

--- BigInt.cpp
+++ BigInt.cpp
@@ -3,20 +3,19 @@
 
 using namespace std;
 
-
 // Checks if the string is a number or other invalid characters were used. 
 // If the first char is '-' then check starting from 1st char
 // Otherwise from the 0th
-bool IsNumber(string number, int size_n) {
+bool IsNumber(string num, int num_len) {
 
     bool answer;
 
     int i = -1; 
 
-    i = (number[0] == '-') ? 1 : 0;
+    i = (num[0] == '-') ? 1 : 0;
 
-    for(i; i < size_n; i++) {
-        if ((number[i] < '0') || (number[i] > '9'))
+    for(i; i < num_len; i++) {
+        if ((num[i] < '0') || (num[i] > '9'))
             answer = false;
         else                
             answer = true;
@@ -26,18 +25,149 @@ bool IsNumber(string number, int size_n) {
 
 
 
+string Pos_to_Neg (string num, int num_len) {
+
+    string new_num = "-";
+    new_num.resize(num_len + 1);
+
+    for (int i = 0; i < num_len; i++) {
+        new_num[i+1] = num[i];
+    }
+    return new_num;
+}
+
+
+string Neg_to_Pos (string num, int num_len) {
+
+    string new_num = num;
+    for (int i = 1; i < num_len; i++){
+        new_num[i-1] = num[i];
+    }
+    new_num.resize(num_len - 1);
+    return new_num;
+}
+
+
+
+char getMaxMagnitude(string num_1, string num_2, int len_1, int len_2, bool sign_1, bool sign_2){
+
+    char answer;
+
+    if (!(sign_1 ^ sign_2)) {
+
+        int difference = len_1 - len_2;
+
+        if (difference == 0) {
+            bool checker = 0;
+            int i = 0;
+            while (checker && i < len_1) {
+                if (num_1[i] == num_2[i]) {
+                    i++;
+                }
+                else if (num_1[i] > num_2[i]) {
+                    checker = false;
+                    answer = '1';
+                }
+                else if (num_2[i] > num_1[i]) {
+                    checker = false;
+                    answer = '2';
+                }
+            }
+            
+            if (checker) 
+                answer = '0';
+        }
+        else 
+            answer = (difference > 0) ? '1' : '2';
+    }
+
+    else if (sign_1 && !sign_2) {
+        int difference = len_1 - (len_2 - 1);
+
+        if (difference == 0) {
+            bool checker = 0;
+            int i = 0;
+            while (checker && i < len_1) {
+                if (num_1[i] == num_2[i+1]) {
+                    i++;
+                }
+                else if (num_1[i] > num_2[i+1]) {
+                    checker = false;
+                    answer = '1';
+                }
+                else if (num_2[i] > num_1[i+1]) {
+                    checker = false;
+                    answer = '2';
+                }
+            }
+            
+            if (checker) 
+                answer = '0';
+        }
+        else 
+            answer = (difference > 0) ? '1' : '2';
+    }
+
+    else if (!sign_1 && sign_2) {
+        int difference = (len_1 - 1) - len_2;
+
+        if (difference == 0) {
+            bool checker = 0;
+            int i = 0;
+            while (checker && i < len_2) {
+                if (num_1[i+1] == num_2[i]) {
+                    i++;
+                }
+                else if (num_1[i+1] > num_2[i]) {
+                    checker = false;
+                    answer = '1';
+                }
+                else if (num_2[i+1] > num_1[i]) {
+                    checker = false;
+                    answer = '2';
+                }
+            }
+            
+            if (checker) 
+                answer = '0';
+        }
+        else 
+            answer = (difference > 0) ? '1' : '2';
+
+    }
+    
+}
+string DeleteLeadZeroes(string num, int num_len) {
+    int i = (num[0] == '-') ? 1 : 0;
+    int j = i;
+    bool checker = true;
+
+    while (checker) {
+        if (num[i] == '0') i++;
+        else checker = false;
+    }
+
+    for (i; i < num_len; i++) {
+        num[j] = num[i];
+        j++; 
+    }
+    num.resize(j);
+    return num;
+}
+
+
 // Helping function that performs addition between numbers of the same sign.
 // Creates string "answer" that is 1 byte longer than biggest of the numbers.
 // Sets ending index int at 0 if numbers are positive and 1 otherwise.
 // answer[0] = NULL if both numbers are positive and '-' otherwise.
 
-// i and j are set to c_len-1 and d_len-1, and because c >= d
+// i and j are set to c_len-1 and d_len-1; and because c >= d
 // the loop goes from the end of each strings until j hits end_index first.
 // Loop performs addition, keeps track of carryover, writes result in answer.
-// Once j < 0, another loop continues until i hits end_index.
+// Once j < 0, another loop continues the same until i hits end_index too.
 
-// After finihshing, for loop gets rid of any NULLs in final answer string.
-string Add_Same_Sign(string c, string d, int c_len, int d_len, bool signs){
+// After finihshing, another for loop gets rid of any NULLs in answer.
+string Addition(string c, string d, int c_len, int d_len, bool signs){
     
     string answer = "";
     answer.resize(c_len + 1); 
@@ -45,9 +175,7 @@ string Add_Same_Sign(string c, string d, int c_len, int d_len, bool signs){
     int end_index = (signs == true) ? 0 : 1;
     answer[0] = (signs == true) ? 0 : '-';
 
-    bool carryover = false; 
-    int difference = c_len - d_len;  
-    
+    bool carryover = false;  
     int i = c_len - 1;
     int j = d_len - 1;
 
@@ -75,15 +203,20 @@ string Add_Same_Sign(string c, string d, int c_len, int d_len, bool signs){
         i--;
     }
 
-    if ( i >= end_index && carryover) {
-        answer[i+1] += c[i] + 1;
-        carryover = false;
-        i--;
-    }
-    for (i; i >= end_index; i--) {
-        answer[i+1] = c[i];
-    }
 
+    for (i; i >= end_index; i--) {
+        if (carryover && (c[i] == 9)) {
+            carryover = true;
+            answer[i+1] = '0';
+        }
+        else if (carryover) {
+            carryover = false;
+            answer[i+1] = c[i] + 1;
+        }
+        else {
+            answer[i+1] = c[i];
+        }    
+    }
 
 
     for (int i = 0; i < answer.length(); i++) 
@@ -94,16 +227,67 @@ string Add_Same_Sign(string c, string d, int c_len, int d_len, bool signs){
 
 }
 
-string Add_Diff_Sign(string a, string b) {
 
+
+string Subtraction(string c, string d, int c_len, int d_len) {
+    string answer = c;
+
+    bool carryover = false;  
+    int i = c_len - 1;
+    int j = d_len - 1;
+
+    for (j; j >= 0; j--) {
+
+        char temp = c[i] - d[j];
+
+        if ((carryover == false) && (temp >= 0)) {
+            answer[i] = temp + 48;
+            carryover = false;
+        }
+        else if ((carryover == false) && (temp < 0)) {
+            answer[i] = temp + 58;
+            carryover = true;
+        }
+        else if ((carryover == true) && (temp >= 1)) {
+            answer[i] = temp + 47;
+            carryover = true;
+        }
+        else if ((carryover == true) && (temp < 1)) {
+            answer[i] = temp + 57;
+            carryover = false;
+        }
+
+        i--;
+    }
+
+    for (i; i >= 0; i--) {
+
+        if (carryover && (c[i] == '0')){
+            carryover = true;
+            answer[i] = '9';
+        }
+        else if (carryover) {
+            carryover = false;
+            answer[i] = c[i] - 1;
+        }
+        else {   
+            answer[i] = c[i];
+        }
+    }
+
+    //insert check for if leading zero
+    answer = DeleteLeadZeroes(answer, c_len);
+    return answer;
 }
+
+
 
 // The addition function.
 // Checks whether inputs are numbers and if so then proceeds.
 // Checks whether inputs are negative.
-// If both numbers' have the same sign, call Add_Same_Sign function.
-// The biggest magnitute of two numbers is passed first.
-string BigIngCalc::Add(string a, string b){
+// If both numbers' have the same sign, call Addition function.
+// The number with the biggest absolute magnitude is passed first.
+string BigIntCalc::Add(string a, string b){
 
     string answer;
     int a_len = a.length();
@@ -114,35 +298,140 @@ string BigIngCalc::Add(string a, string b){
         return answer;
     }
 
-    int aBigger = a_len - b_len;
+    bool sign_a = (a[0] == '-') ? false : true;
+    bool sign_b = (b[0] == '-') ? false : true; 
+
+    char which_bigger = getMaxMagnitude(a, b, a_len, b_len, sign_a, sign_b);
+
+    if(!(sign_a ^ sign_b)) {
+
+        if (sign_a && sign_b) {
+            if (which_bigger == '1' || which_bigger == '0') 
+                answer = Addition(a, b, a_len, b_len, true);
+            else if (which_bigger == '2')
+                answer = Addition(b, a, b_len, a_len, true);
+        }
+
+        else if (!sign_a  && !sign_b) {
+            if (which_bigger == '1' || which_bigger == '0') 
+                answer = Addition(a, b, a_len, b_len, false);
+            else if (which_bigger == '2')
+                answer = Addition(b, a, b_len, a_len, false);
+        }
+    }
+
+    else {
+        if (which_bigger == '0') {
+            answer = "0";
+            return answer;
+        }
+
+        if (sign_a & !sign_b) {
+            if (which_bigger == '1') {
+                answer = Subtraction(a, b, a_len, b_len);
+            }
+            else if (which_bigger == '2') {
+                //todo
+            }
+        }
+        else if (!sign_a && sign_b) {  
+            if (which_bigger == '1') {
+                answer = Subtraction(a, b, a_len, b_len);
+            }
+            else if (which_bigger == '2') {
+                //todo
+            }
+        }
+        
+    
+    }
+
+    return answer;
+}
+
+
+// The substraction function
+// Checks whether inputs are numbers and if so then proceeds.
+// Checks numbers' signs and if they are different 
+// Changes second to match first and calls Addition function
+
+string BigIntCalc::Subtract(string a, string b) {
+
+    string answer;
+    int a_len = a.length();
+    int b_len = b.length();
+
+    if (!IsNumber(a, a_len) || !IsNumber(b, b_len)) {
+        answer = "Error: invalid symbols";
+        return answer;
+    }
 
     bool sign_a = (a[0] == '-') ? false : true;
     bool sign_b = (b[0] == '-') ? false : true; 
 
-    if (sign_a && sign_b) {
-        if (aBigger >= 0) 
-            answer = Add_Same_Sign(a, b, a_len, b_len, true);
-        else
-            answer = Add_Same_Sign(b, a, b_len, a_len, true);
+    if (sign_a ^ sign_b) {
+
+        if (sign_a && !sign_b) {
+
+            b = Neg_to_Pos(b, b_len);
+            char which_bigger = getMaxMagnitude(a, b, a_len, b_len - 1, sign_a, sign_b);
+
+            if (which_bigger == '1' || which_bigger == '0') 
+                answer = Addition(a, b, a_len, b_len - 1, true);
+            else if (which_bigger == '2')
+                answer = Addition(b, a, b_len - 1, a_len, true);
+        }
+
+        else if (!sign_a && sign_b) {
+            b = Pos_to_Neg(b, b_len);
+            char which_bigger = getMaxMagnitude(a, b, a_len, b_len + 1, sign_a, sign_b);
+
+            if (which_bigger == '1' || which_bigger == '0')
+                answer = Addition(a, b, a_len, b_len + 1, false);
+            else if (which_bigger == '2')
+                answer = Addition(b, a, b_len + 1, a_len, false);
+        }
     }
 
-    else if (!sign_a  && !sign_b) {
-        if (aBigger >= 0) 
-            answer = Add_Same_Sign(a, b, a_len, b_len, false);
-        else
-            answer = Add_Same_Sign(b, a, b_len, a_len, false);
+    else {
+
+        char which_bigger = getMaxMagnitude(a, b, a_len, b_len, sign_a, sign_b);
+
+        if (which_bigger == '0') {
+            answer = "0";
+            return answer;
+        }
+
+        if (sign_a && sign_b) {
+            if (which_bigger == '1') {
+                answer = Subtraction(a, b, a_len, b_len);
+            }
+            else if (which_bigger == '2') {
+                //поменять знак ответа
+            }
+        }
+
+        else if (!sign_a && !sign_b) {
+            if (which_bigger == '1') {
+                answer = Subtraction(a, b, a_len, b_len);
+            }
+            else if (which_bigger == '2') {
+                //поменять знак ответа
+            }
+        }
     }
+
     return answer;
 }
 
-string BigIngCalc::Subtract(string a, string b){
+
+
+string BigIntCalc::Multiply(string a, string b){
 
 }
 
-string BigIngCalc::Multiply(string a, string b){
 
-}
 
-string BigIngCalc::Divide(string a, string b){
+string BigIntCalc::Divide(string a, string b){
 
 }
--- BigInt.h
+++ BigInt.h
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-    class BigIngCalc {   
+    class BigIntCalc {   
         public:
 
         static string Add(string a, string b);
